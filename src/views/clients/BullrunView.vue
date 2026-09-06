72108cb45cbd3b3ed0ed4aaeb9b5661715a6f91b
RedDev <47223194+RedWillDev@users.noreply.github.com>
2026-09-06 20:05:14 +0200

    Add named client project views

---
+++ src/views/clients/BullrunView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import ProjectCaseStudy from '../../components/projects/ProjectCaseStudy.vue'
+import { bullrun } from '../../data/projects/bullrun'
+</script>
+
+<template>
+  <ProjectCaseStudy :project="bullrun" />
+</template>
