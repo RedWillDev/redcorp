ab737e1e660f60674f7bcad66b46371144a00384
RedDev <47223194+RedWillDev@users.noreply.github.com>
2026-09-06 20:05:23 +0200

    Add named client project views

---
+++ src/views/clients/MhaniView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import ProjectCaseStudy from '../../components/projects/ProjectCaseStudy.vue'
+import { mhani } from '../../data/projects/mhani'
+</script>
+
+<template>
+  <ProjectCaseStudy :project="mhani" />
+</template>
